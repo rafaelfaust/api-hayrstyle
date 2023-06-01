1358c6dc102b1024e7446e78e29d4b44ac337d0d
Rafael Faust Rodrigues <106990982+rafaelfaust@users.noreply.github.com>
2023-05-31 21:49:31 -0300

    Finalizado

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,17 @@
+import { Router, request } from 'express'
+import { UsersController } from '../controllers/UsersController'
+class UserRoutes {
+    private router: Router
+    private usersController: UsersController
+    constructor() {
+        this.router = Router()
+        this.usersController = new UsersController
+    }
+    getRoutes(){
+        this.router.post('/', this.usersController.store.bind(this.usersController))
+
+        return this.router
+    }
+}   
+
+export { UserRoutes }
